d77d7f2927668bee77c0d670aec14fbcb64c2299
Bardent <37575819+Bardent@users.noreply.github.com>
2025-04-15 12:14:47 -0400

    chore: setup cleanup

--- src/app.js
+++
@@ -1,3 +0,0 @@
-export function someFunction(a, b) {
-  return a + b;
-}
--- src/app.test.js
+++
@@ -1,7 +0,0 @@
-import { someFunction } from './app';
-
-test('test test', () => {
-  expect(someFunction(1, 2)).toBe(3);
-  expect(someFunction(2, 5)).toBe(7);
-  expect(someFunction(2, 6)).toBe(8);
-});
