d51a11698dffcf87171d0b01d2ed4ed1778ed3b1
Heinrich <37575819+Bardent@users.noreply.github.com>
2025-04-15 12:19:42 -0400

    Project setup (#1)
    * chore: add gitignore file
    
    * chore: initialize npm with -y option
    
    * chore: install webpack dev dependency
    
    Used command npm i -D webpack webpack-cli
    
    * feat: create src directory with basic files
    
    Basic files used to confirm setup is working as intended
    
    * chore: update gitignore to ignore dist directory
    
    * chore: setup webpack config and build npm script
    
    * chore: install html-webpack-plugin dev dependency
    
    Plugin should allow automatic inclusion of assets in app.html
    
    * chore: update webpack config to use html plugin
    
    Now when webpack build command is called, app.html is used as the
    template for the bundled html file with main.js automatically included
    in the script element. main.js is the bundles js using app.js as the
    entry point.
    
    * chore: update webpack conf. to clean dist dir
    
    * chore: install dev server and config watch
    
    Can now use comman 'npm start' to automatically start dev server
    that will watch for changes and automatically rebundle and reload
    
    * chore: install eslint
    
    Followed:
    - https://www.youtube.com/watch?v=eieTlMwCwWU
    - https://eslint.org/docs/latest/use/getting-started
    
    * chore: Update eslint config to work with node
    
    * chore: install prettier
    
    * chore: add settings for prettier and eslint
    
    * chore: install jest testing framework
    
    * chore: add jest ESM support
    
    * chore: confirm testing with jest works
    
    Run npm test
    
    * chore: update eslint config to work with jest
    
    * chore: install jest types for autocomplete
    
    * chore: add automatic testing
    
    Use npm run watch to start automatic testing
    
    * chore: setup cleanup

---
+++ eslint.config.mjs
@@ -0,0 +1,24 @@
+import js from '@eslint/js';
+import globals from 'globals';
+import { defineConfig } from 'eslint/config';
+import eslintPluginPrettier from 'eslint-plugin-prettier/recommended';
+import eslintConfigPrettier from 'eslint-config-prettier/flat';
+
+export default defineConfig([
+  {
+    files: ['**/*.{js,mjs,cjs}'],
+    languageOptions: { globals: { ...globals.node, ...globals.jest } },
+  },
+  {
+    files: ['**/*.{js,mjs,cjs}'],
+    plugins: { js },
+    extends: ['js/recommended'],
+  },
+  {
+    rules: {
+      'no-unused-vars': 'warn',
+    },
+  },
+  eslintConfigPrettier,
+  eslintPluginPrettier,
+]);
